7025f1acab75e8a065c8c6b9b946130c90bb32e9
Paul Njoroge <198077751+paulmwash@users.noreply.github.com>
2026-06-14 16:05:35 +0300

    Adding comments

--- file_type.sh
+++ file_type.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#This code prompts user for input file 
+#stores it in a variable 
+#checks if file exists if exists
+#its checks its type
+
 
 echo "Enter file to determine its type"
 read -p "Enter file " file
