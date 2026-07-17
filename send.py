3f711a10e820609bfdab0e10f70c525b10aee520
Paul <198077751+paulmwash@users.noreply.github.com>
2026-07-17 09:17:00 +0300

    I can code lol

--- send.py
+++ send.py
@@ -2,7 +2,7 @@
 
 import send2trash,os
 
-os.chdir("/home/paul-njoroge/COPIED")
+os.chdir("/home/paul-njoroge/COPIED") #Relplace with your location
 
 for file in os.listdir('.'):
    send2trash.send2trash(file)
