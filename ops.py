c4683eaeee108d2c015af15c73a690e7c374f3c3
Paul <198077751+paulmwash@users.noreply.github.com>
2026-07-21 13:56:41 +0300

    I can code lol

--- ops.py
+++ ops.py
@@ -3,21 +3,21 @@
 
 import os,shutil
 
+def operations():
+  SOURCE_DIR="/home/paul-njoroge/Documents"
+  DEST_DIR="/home/paul-njoroge/MUST"
 
-SOURCE_DIR="/home/paul-njoroge/Documents"
-DEST_DIR="/home/paul-njoroge/MUST"
+  if not os.path.exists(DEST_DIR):
+    os.makedirs(DEST_DIR)
+    print(f"Dir {DEST_DIR} created successful") 
+  else:
+    print(f"Dir {DEST_DIR} exists ")
 
-if not os.path.exists(DEST_DIR):
-  os.makedirs(DEST_DIR)
-  print(f"Dir {DEST_DIR} created successful") 
-else:
-  print(f"Dir {DEST_DIR} exists ")
+  os.chdir(SOURCE_DIR)
 
-os.chdir(SOURCE_DIR)
-
-for file in os.listdir(SOURCE_DIR):
-  if file.endswith(".pdf"):
-   source_file=os.path.join(SOURCE_DIR,file)
-   dest_file=os.path.join(DEST_DIR,file)
-   shutil.copy(source_file,dest_file)
-print(os.getcwd())
+  for file in os.listdir(SOURCE_DIR):
+    if file.endswith(".pdf"):
+     source_file=os.path.join(SOURCE_DIR,file)
+     dest_file=os.path.join(DEST_DIR,file)
+     shutil.copy(source_file,dest_file)
+  print(os.getcwd())
