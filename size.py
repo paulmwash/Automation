c73ff2eb9b0bdb94791cf6229a018922297363b8
Paul <198077751+paulmwash@users.noreply.github.com>
2026-07-21 13:52:37 +0300

    I can code lol

--- size.py
+++ size.py
@@ -3,7 +3,11 @@
 
 import os
 
-total=0
-for filename in os.listdir("/home/paul-njoroge/SCRIPTS"):
-    total= total + os.path.getsize(os.path.join("/home/paul-njoroge/SCRIPTS",filename))
-print(total)
+def get_size():
+  total=0
+  for filename in os.listdir("/home/paul-njoroge/SCRIPTS"):
+     total= total + os.path.getsize(os.path.join("/home/paul-njoroge/SCRIPTS",filename))
+  print(total)
+
+if __name__=="__main__":
+    get_size()
